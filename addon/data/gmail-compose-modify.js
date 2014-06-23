964bc82d7dec5938bb9e6a1f1a1516e0b88869da
Aabha Biyani <abiyani7@gatech.edu>
2014-06-23 15:30:13 -0700

    Fix for https://github.com/pi5/FirefoxSimpleAddon/issues/1

--- addon/data/gmail-compose-modify.js
+++ addon/data/gmail-compose-modify.js
@@ -1,13 +1,27 @@
 // Fetch the first compose window. TODO:Refine the way the compose window is fetched
-var composeWindows = document.querySelectorAll('div.editable.Am.Al');
-var iframeContainer = composeWindows[0];
-var iframe = iframeContainer.childNodes[0];
-var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+//var tabs = require("sdk/tabs");
 
-var iconDiv = innerDoc.createElement('div');
-// TODO: Fetch images from current source instead of external link
-iconDiv.innerHTML = '<img src="http://cdn-static.zdnet.com/i/story/60/52/001470/firefox-logo_small.png" height="96" width="96"><br>';
+var urlMatch = /.*mail.google.com.*compose=.*/
+var currentTabUrl = document.location.href;
+
+if (currentTabUrl.match(urlMatch)) {
+	var composeWindows = document.querySelectorAll('div.editable.Am.Al');
+	var iframeContainer = composeWindows[0];
+	var iframe = iframeContainer.childNodes[0];
+	var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+	var iconDiv = innerDoc.createElement('div');
+	// TODO: Fetch images from current source instead of external link
+	iconDiv.innerHTML = '<img src="http://cdn-static.zdnet.com/i/story/60/52/001470/firefox-logo_small.png" height="96" width="96"><br>';
+
+	// TODO: Prepend instead of appending. Note: If the image is prepended, it does not appear after email is received.
+
+	console.log(currentTabUrl);
+		innerDoc.body.appendChild(iconDiv);
+		console.log("Bugs bunny says yay!!");
+	}
+else {
+	console.log("Bugs bunny says booo :(");
+}
 
 
-// TODO: Prepend instead of appending. Note: If the image is prepended, it does not appear after email is received.
-innerDoc.body.appendChild(iconDiv);
--- addon/lib/main.js
+++ addon/lib/main.js
@@ -19,4 +19,3 @@ function handleClick(state) {
 	});
 }
 
-
